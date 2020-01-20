f504b6c8281863d495453cad20ead9413fed3a3b
fedPass <federica.passanante@gmail.com>
2020-01-20 17:00:30 +0100

    censare testo con badword

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 // Creare una variabile che contiene del testo
-$text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>';
+$text_original = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<hr>';
+echo ('il testo originale è <br>' . $text_original);
 
 //Leggere dal parametro in GET una badword
 $badword = $_GET['badword'];
 echo ('la parola da censurare è: ' . $badword);
 
 //cercare all'interno del testo la badword e sostituirla con "***"
+$text_edited = str_replace($badword, '***', $text_original);
 
 //visualizzare a schermo il paragrafo aggiornato
  ?>
 
- <p><?php echo $text ?></p>
+ <p><?php echo $text_edited ?></p>
